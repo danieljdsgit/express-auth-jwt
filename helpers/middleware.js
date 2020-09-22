6957c5ddcdb9f72229f5cd678dc406a6f64e9c75
Daniel <danieljds@gmail.com>
2020-09-22 12:06:53 +0200

    middleware refactor

--- helpers/middleware.js
+++ helpers/middleware.js
@@ -2,30 +2,30 @@ const jwt = require("jsonwebtoken");
 
 const secret = process.env.SECRET_SESSION;
 
-const withAuth = function (req, res, next) {
-  // obtenemos el token de las cookies
-  const token = req.cookies.token;
-  // si no hay token, seteamos el valor de la variable isUserLoggedIn en false y pasamos el control a la siguiente función de middleware
-  if (!token) {
+const withAuth = async (req, res, next) => {
+  try {
+    // obtenemos el token de las cookies
+    const token = req.cookies.token;
+    // si no hay token, seteamos el valor de la variable isUserLoggedIn en false y pasamos el control a la siguiente función de middleware
+    if (!token) {
+      res.locals.isUserLoggedIn = false;
+      next();
+    } else {
+      // verificamos el token
+      const decoded = await jwt.verify(token, secret);
+
+      // si el token valida, configuramos req.user con el valor del decoded userWithoutPass
+      req.user = decoded.userWithoutPass;
+      console.log(req.user);
+      res.locals.currentUserInfo = req.user;
+      res.locals.isUserLoggedIn = true;
+      next();
+    }
+  } catch (err) {
+    // si hay un error, configuramos el valor de la variable isUserLoggedIn en false y pasamos el control a la siguiente ruta
+    console.error(err);
     res.locals.isUserLoggedIn = false;
-    next();
-  } else {
-    // verificamos el token
-    jwt.verify(token, secret, function (err, decoded) {
-      if (err) {
-        res.locals.isUserLoggedIn = false;
-        // si hay un error, renderizamos la home y devolvemos un mensaje
-        res.status(401).render("home", {
-          errorMessage: "Unauthorized: No token provided",
-        });
-      } else {
-        // si el token valida, configuramos req.user con el valor del decoded userWithoutPass
-        req.user = decoded.userWithoutPass;
-        res.locals.currentUserInfo = req.user;
-        res.locals.isUserLoggedIn = true;
-        next();
-      }
-    });
+    next(err);
   }
 };
 
